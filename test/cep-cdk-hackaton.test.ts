6392a37a72aae7620cb04549cf4ad53fcfabf4b7
Alessandro Vago <alessandro.vago@nn-group.com>
2022-11-17 18:43:01 +0100

    Initial commit

---
+++ test/cep-cdk-hackaton.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CepCdkHackaton from '../lib/cep-cdk-hackaton-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cep-cdk-hackaton-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CepCdkHackaton.CepCdkHackatonStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
